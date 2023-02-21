a8564ce684a6d694a08c38abea08637d8063dd3e
Ciarán J.M <macdonald.cjm@gmail.com>
2023-02-21 12:41:29 +0000

    dependencies added

---
+++ scripts/WebSCrape.py
@@ -0,0 +1,13 @@
+import requests
+import nltk
+import math
+import re
+import spacy
+import regex as re
+import pandas as pd
+import numpy as np
+import statistics as stats
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import json
+
